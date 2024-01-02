6bcb35203a2e32f2c4da62e1e2883d060397a3d2
Salxchange <138751618+Salxchange@users.noreply.github.com>
2024-01-02 21:27:04 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,7 +53,7 @@ async def op(_, m :Message):
                 ]
             )
             add_user(m.from_user.id)
-            await m.reply_photo("https://telegra.ph/file/63d723680cca52ba46319.jpg", caption="**{}\nI'm an auto approve [Admin Join Requests]({}) Bot.\nI can approve users in Groups/Channels.Add me to your Channel Or Group and promote me Admin with add members permission.\n\n__By : @Rokubotz__**".format(m.from_user.mention, "https://telegram.me/Rokubotz"), reply_markup=keyboard)
+            await m.reply_photo("https://telegra.ph/file/27f1d1afecd9884bb76fb.jpg", caption="**{}\nI'm an auto approve [Admin Join Requests]({}) Bot.\nI can approve users in Groups/Channels.Add me to your Channel Or Group and promote me Admin with add members permission.\n\n__By : @Rokubotz__**".format(m.from_user.mention, "https://telegram.me/Rokubotz"), reply_markup=keyboard)
     
         elif m.chat.type == enums.ChatType.GROUP or enums.ChatType.SUPERGROUP:
             keyboar = InlineKeyboardMarkup(
